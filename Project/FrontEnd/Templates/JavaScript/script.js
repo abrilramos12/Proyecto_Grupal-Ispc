ac3a5a4a53a6e9ac5e736b8addda8af650795fe2
Gaby <gaby_ag@live.com.ar>
2022-11-03 09:24:43 -0300

    validacion form login con js

--- Project/FrontEnd/Templates/JavaScript/script.js
+++ Project/FrontEnd/Templates/JavaScript/script.js
@@ -1,3 +1,15 @@
-function mensaje() {
-    alert("Bienvenido a JS")
-}
+
+function validateForm() {
+    let x = document.forms["myForm"]["username"].value;
+    let y = document.forms["myForm"]["password"].value;
+    if (x == "") {
+      alert("Debe ingresar un usuario");
+      return false;
+    }
+    if (y == "") {
+        alert("Debe ingresar una contraseña");
+        return false;
+      }
+
+  }
+  
