07484a85d866ad91c2e06829c4a51e7a5f5edbf0
Gaby Aguilera <gaby_ag@live.com.ar>
2023-05-01 13:38:56 -0300

    agrego componente carrito

---
+++ Front-end/CapacitApp/src/app/carrito/carrito.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrito',
+  templateUrl: './carrito.component.html',
+  styleUrls: ['./carrito.component.css']
+})
+export class CarritoComponent {
+
+}
