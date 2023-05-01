9241d7fffb99b6892bcc5bd86fe9715de16bb997
EdithMoyano <moyanoe357@gmail.com>
2023-05-01 12:01:05 -0300

    creo componente footer

--- Front-end/CapacitApp/src/app/app.module.ts
+++ Front-end/CapacitApp/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
