4601a31c9d7b74915953fa923e6456c002d64a6f
Ramos Nicolás <jnicolas.ramos10@gmail.com>
2023-05-03 04:01:28 -0300

    Agrego home component para probar hacer un enrutado. Cambio algunos estilos para resaltarlos. Agrego Bootstrap y lo configuro en angular.json para poder utilizarlo

--- Front-end/CapacitApp/src/app/app-routing.module.ts
+++ Front-end/CapacitApp/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RegisterComponent } from './register/register.component';
 import { CarritoComponent } from './carrito/carrito.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 const routes: Routes = [
-  {path: '', redirectTo:'/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo:"/home", pathMatch: "full"},
+  {path:'home', component: HomeComponent},
+  {path: 'perfil', component: PerfilComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'cart', component: CarritoComponent},
   {path: '**', component: PageNotFoundComponent},
