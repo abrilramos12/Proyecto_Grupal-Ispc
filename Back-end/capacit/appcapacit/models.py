3b901f9dda17dfce9dada68276b9361e5fe5abcc
EdithMoyano <moyanoe357@gmail.com>
2023-05-19 16:58:08 -0300

    creo tablas Student y Video en models.py

--- Back-end/capacit/appcapacit/models.py
+++ Back-end/capacit/appcapacit/models.py
@@ -2,14 +2,31 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    id_user = models.CharField (max_length=256)
+    id_user = models.AutoField(primary_key=True)
     first_name = models.CharField (max_length=45)
     last_name = models.CharField (max_length=45)
-    username = models.CharField (max_length=16)
-    email = models.CharField (max_length=256)
+    username = models.CharField (max_length=45)
+    email = models.EmailField()
     password = models.CharField (max_length=32)
+    activate = models.BooleanField()    
     role = models.CharField(max_length=45)
-    reate_time= models.DateTimeField (auto_now_add=True)
-    user_git = models.CharField (max_length=128)
-    user_linkedin = models.CharField (max_length=128)
+    create_time= models.DateTimeField (auto_now_add=True)
+    user_git = models.CharField (max_length=45)
+    user_linkedin = models.CharField (max_length=100)
+
+class Student(models.Model):
+    id_student = models.AutoField(primary_key=True)  
+    id_user = models.ForeignKey()
+    dni = models.IntegerField()
+    user_vip = models.BooleanField()
+
+class Video(models.Model):
+    id_video = models.AutoField(primary_key=True)
+    descripcion = models.TextField (max_length=500)
+    blob = models.TextField ()
+    id_course = models. ForeignKey ()
+    id_teacher = models.ForeignKey()
+    link = models.CharField ( max_length=45)
+    
+
 
