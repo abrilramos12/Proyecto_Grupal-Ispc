4b4435ee6986f4dcd60847dd8061b3c236ba2893
Daniela Barrios <dani_bj1@hotmail.com>
2023-05-20 13:57:16 -0300

    agrego clase Teacher y Couras

--- Back-end/capacit/appcapacit/models.py
+++ Back-end/capacit/appcapacit/models.py
@@ -8,7 +8,7 @@ class User(models.Model):
     username = models.CharField (max_length=45)
     email = models.EmailField()
     password = models.CharField (max_length=32)
-    activate = models.BooleanField()    
+    activate = models.BooleanField(default=False)    
     role = models.CharField(max_length=45)
     create_time= models.DateTimeField (auto_now_add=True)
     user_git = models.CharField (max_length=45)
@@ -16,7 +16,7 @@ class User(models.Model):
 
 class Student(models.Model):
     id_student = models.AutoField(primary_key=True)  
-    id_user = models.ForeignKey(User, to_field="id_user")
+    id_user = models.ForeignKey(User, to_field="id_user", on_delete=models.CASCADE)
     dni = models.IntegerField()
     user_vip = models.BooleanField()
 
@@ -24,8 +24,8 @@ class Video(models.Model):
     id_video = models.AutoField(primary_key=True)
     descripcion = models.TextField (max_length=500)
     blob = models.TextField ()
-    id_course = models. ForeignKey ()
-    id_teacher = models.ForeignKey()
+    id_course = models. ForeignKey ("Course", on_delete=models.CASCADE)
+    id_teacher = models.ForeignKey("Teacher", on_delete=models.CASCADE)
     link = models.CharField ( max_length=45)
 
 class Sale(models.Model):
@@ -43,5 +43,22 @@ class Payment(models.Model):
     date = models.DateField(null=False, auto_now_add=True)
     total = models.FloatField(null=False)
     id_sale = models.ForeignKey("Sale", on_delete=models.CASCADE)
+
+
+class Teacher(models.Model):
+    id_teacher = models.AutoField(primary_key=True)  
+    id_user = models.ForeignKey(User, to_field="id_user", on_delete=models.CASCADE)
+    dni = models.IntegerField()
+
+
+class Course(models.Model):
+    id_course = models.AutoField(primary_key=True)
+    name = models.CharField (max_length=45)
+    languaje = models.CharField (max_length=45)
+    tag_1 = models.CharField (max_length=45)
+    tag_2 = models.CharField (max_length=45)
+    link = models.CharField (max_length=45)
+    id_teacher = models.ForeignKey("Teacher", on_delete=models.CASCADE)
+    
 
 
